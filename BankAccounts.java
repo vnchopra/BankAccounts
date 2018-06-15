a12abb430912580ada49e6d822d9c163eb370ce1
Varun Chopra <vnchopra@gmail.com>
2018-06-14 18:57:28 -0700

    Updated formatting.

--- BankAccounts.java
+++ BankAccounts.java
@@ -1,10 +1,11 @@
 import java.util.*;
 import java.util.ArrayList;
 
-public class BankAccounts {
+public class BankAccounts 
+{
 	
-	static String feeOrUpfront(int n, int k, int x, int d, int[] p) {
-		
+	static String feeOrUpfront(int n, int k, int x, int d, int[] p) 
+	{
         // Complete this function
 		String upFront = "upfront";
 		String fee = "fee";
@@ -12,16 +13,19 @@ public class BankAccounts {
 
 		BankAccounts solution = new BankAccounts();	
 		
-		for (int j = 0; j < p.length; j++) {
+		for (int j = 0; j < p.length; j++) 
+		{
 			transactionFeeArray.add(solution.simpleValueCalculator(k, x, p[j]));
 		}
 		
 		float addedTransactionFee = solution.addAllValuesInList(transactionFeeArray);
 		
-		if (addedTransactionFee == d) {
+		if (addedTransactionFee == d) 
+		{
 			return fee;
 		}
-		else if (addedTransactionFee > d) {
+		else if (addedTransactionFee > d) 
+		{
 			return upFront;
 		}
 		else
@@ -31,16 +35,19 @@ public class BankAccounts {
 		
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner in = new Scanner(System.in);
         int q = in.nextInt(); //Number of scenarios to handle
-        for(int a0 = 0; a0 < q; a0++){
+        for(int a0 = 0; a0 < q; a0++)
+        {
             int n = in.nextInt(); //Number of payments
             int k = in.nextInt(); //Would charge this if % isnt past this value
             int x = in.nextInt(); //Percent charged per pi
             int d = in.nextInt(); //Upfront payment amount
             int[] p = new int[n];
-            for(int p_i = 0; p_i < n; p_i++){
+            for(int p_i = 0; p_i < n; p_i++)
+            {
                 p[p_i] = in.nextInt(); //a payment amount which can be a certain number
             }
             String result = feeOrUpfront(n, k, x, d, p);
@@ -52,7 +59,8 @@ public class BankAccounts {
     private float addAllValuesInList(List<Float> arrayList)
     {
     	float addedTransactions = 0.0f;
-    	for (Float transactFee : arrayList) {
+    	for (Float transactFee : arrayList) 
+    	{
     		addedTransactions = addedTransactions + transactFee;
 		}
     	return addedTransactions;
